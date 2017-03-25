8352a4fffb5b881b2e405683696335ebfde1da76
Nicholas Burns <nrburns.1@gmail.com>
2017-03-25 21:09:54 +1100

    Section_04: Setup Player and AI tank controller classes

---
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankAIController.h"
+
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ATank* ControlledTank = GetControlledTank();
+	if (ControlledTank != nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController possessing %s"), *(ControlledTank->GetName()));
+	}
+}
+
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
